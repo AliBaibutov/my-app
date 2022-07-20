b5281602eb5a7f9c4b7a10e2cadd40de59872f8c
AliKhan <baybutov95@mail.ru>
2022-07-20 22:52:45 +0500

    приложение за преподом 2

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,54 +1,43 @@
 import React, { useState } from "react";
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
-  const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
+const Counter = (props) => {
+  const { value } = props;
 
-  const formatCount = () => {
-    return count === 0 ? "empty" : count;
+  const formatValue = () => {
+    return value === 0 ? "empty" : value;
   };
   const getBageClasses = () => {
     let classes = "badge m-2 ";
-    classes += count === 0 ? "bg-warning" : "bg-primary";
+    classes += value === 0 ? "bg-warning" : "bg-primary";
     return classes;
   };
 
   const handleIncrement = () => {
-    setCount((prevState) => prevState + 1);
+    // setValue((prevState) => prevState + 1);
+    console.log("handle increment");
   };
   const handleDecrement = () => {
-    setCount((prevState) => prevState - 1);
+    // setValue((prevState) => prevState - 1);
+    console.log("handle decrement");
   };
-  const handleTagChange = (id) => {
-    setTags((prevState) => prevState.filter((tag) => tag !== id));
-  };
-  const renderTags = () => {
-    return (
-      tags.length !== 0 &&
-      tags.map((tag) => (
-        <li
-          key={tag}
-          className="btn btn-primary btn-sm m-2"
-          onClick={() => handleTagChange(tag)}
-        >
-          {tag}
-        </li>
-      ))
-    );
-  };
-  if (tags.length !== 0) {
-    return <ul>{renderTags()}</ul>;
-  }
+
   return (
-    <>
-      <span className={getBageClasses()}>{formatCount()}</span>
+    <div>
+      <span>{props.name}</span>
+      <span className={getBageClasses()}>{formatValue()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +
       </button>
       <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
         -
       </button>
-    </>
+      <button
+        className="btn btn-danger btn-sm m-2"
+        onClick={() => props.onDelete(props.id)}
+      >
+        Delete
+      </button>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
+import App from "./app";
 
-const App = () => {
-  return <Counter />;
-};
-
-// reactDOM.render(<App />, document.getElementById("root"));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
